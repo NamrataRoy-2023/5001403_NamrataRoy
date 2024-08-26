4e0c151c75bd62e0b07d26f465d1058ba8d0d194
Namrata Roy <122513359+NamrataRoy-2023@users.noreply.github.com>
2024-08-26 13:18:30 +0530

    Add files via upload

---
+++ Week 5/Exercise 10/BookstoreAPI/src/main/java/com/book/BookstoreAPI/serializer/CustomDateSerializer.java
@@ -0,0 +1,18 @@
+package com.book.BookstoreAPI.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CustomDateSerializer extends JsonSerializer<Date> {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public void serialize(Date date, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(dateFormat.format(date));
+    }
+}
---
+++ Week 5/Exercise 10/BookstoreAPI/src/main/java/com/book/BookstoreAPI/service/BookServiceImpl.java
@@ -0,0 +1,58 @@
+package com.book.BookstoreAPI.service;
+
+import com.book.BookstoreAPI.model.Book;
+import com.book.BookstoreAPI.repository.BookRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @Override
+    public Book updateBook(Long id, Book book) {
+        if (!bookRepository.existsById(id)) {
+            throw new RuntimeException("Book not found");
+        }
+        book.setId(id);
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new RuntimeException("Book not found");
+        }
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Book> getBooks(String title, String author) {
+        return bookRepository.findAll().stream()
+                .filter(book -> (title == null || book.getTitle().equalsIgnoreCase(title)) &&
+                        (author == null || book.getAuthor().equalsIgnoreCase(author)))
+                .collect(Collectors.toList());
+    }
+}
